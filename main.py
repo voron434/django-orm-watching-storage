520c171d9a9ff614f1c97ad3347217bea232fb47
Ilya Osipov <070809010@mail.ru>
2022-12-28 21:16:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import os
 from django.core.management import execute_from_command_line
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-execute_from_command_line('manage.py runserver 0.0.0.0:8000'.split())
+execute_from_command_line('manage.py runserver 0.0.0.0:8000'.split()) 
